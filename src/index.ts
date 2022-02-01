f90d076374b11737d9d50811f205769d6c604da3
alecbass <alec.bassingthwaighte@gmail.com>
2022-02-01 11:25:16 +1000

    Add more options and test functions.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export function getMessage() {
   return "hello!";
 }
+
+export interface User {
+  id: number;
+  email: string;
+}
+
+export function getTestUser(): User {
+  return {
+    id: 1,
+    email: "test@bidhive.com",
+  };
+}
